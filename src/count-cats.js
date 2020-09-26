65080784a71b6fe758ca5185b5601e35db7b3218
ryabykhms <ryabykhms@gmail.com>
2020-09-26 19:33:39 +0300

    fix: add check argument for count-cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,5 +1,8 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(matrix) {
+  if (matrix === undefined || !Array.isArray(matrix)) {
+    return false
+  }
   return matrix.reduce((count, line) => count + line.reduce((sum, current) => (current === "^^" ? ++sum : sum), 0), 0);
 };
