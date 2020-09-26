ba87c8eea129eca8f21a86dbb769af1a10c1373d
ryabykhms <ryabykhms@gmail.com>
2020-09-26 21:11:15 +0300

    feat: solve dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+const isString = (v) => typeof v === "string";
+const isEmptyString = (v) => v.trim().length === 0;
+const isStringNumber = (v) => !isNaN(v);
+
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  const filteredMembers = members.filter(
+    (m) => isString(m) && !isStringNumber(m) && !isEmptyString(m)
+  );
+  const firstSymbolMembers = filteredMembers.map((m) =>
+    m.trim().toUpperCase().charAt(0)
+  );
+  const newName = firstSymbolMembers.sort().reduce((prev, current) => {
+    return prev + current;
+  }, "");
+
+  return newName;
 };
