c70432296a63387d3de449a2a02b21bb28890a88
ryabykhms <ryabykhms@gmail.com>
2020-09-28 14:31:55 +0300

    feat: solve transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,50 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+const COMMANDS = [
+  "--double-next",
+  "--double-prev",
+  "--discard-next",
+  "--discard-prev",
+];
+
+const isCommands = (...args) =>
+  args.filter((v) => COMMANDS.indexOf(v) !== -1).length === args.length;
+
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("Error! Argument is not array!");
+  }
+  const resultArr = arr.slice();
+  resultArr.map((el, i, a) => {
+    if (isCommands(el, resultArr[i + 1])) {
+      resultArr.splice(i, 2);
+    }
+    if (isCommands(el, resultArr[i + 1])) {
+      resultArr.splice(i - 1, 2);
+    }
+    switch (el) {
+      case "--discard-next":
+        if (i < resultArr.length - 1) {
+          resultArr.splice(i + 1, 1);
+        }
+        break;
+      case "--discard-prev":
+        if (i > 0) {
+          resultArr.splice(i - 1, 1);
+        }
+        break;
+      case "--double-next":
+        if (i < resultArr.length - 1) {
+          resultArr[i] = resultArr[i + 1];
+        }
+        break;
+      case "--double-prev":
+        if (i > 0) {
+          resultArr[i] = resultArr[i - 1];
+        }
+        break;
+    }
+  });
+
+  return resultArr.filter((v) => !isCommands(v));
 };
