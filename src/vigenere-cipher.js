b94e020188d5f72dd4c03e802d12979f785df4fa
ryabykhms <ryabykhms@gmail.com>
2020-09-28 19:24:56 +0300

    feat: solve vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,61 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
+  }
+  getLatinUpperCharIndex(l, firstUpperCode) {
+    return l.toUpperCase().charCodeAt(0) - firstUpperCode;
+  }
+  calcCodeCipherSymbol(chr, keyChr, firstUpperCode, alphabetSize) {
+    return (
+      firstUpperCode +
+      ((this.getLatinUpperCharIndex(chr, firstUpperCode) +
+        this.getLatinUpperCharIndex(keyChr, firstUpperCode)) %
+        alphabetSize)
+    );
+  }
+  calcDecodeCipherSymbol(chr, keyChr, firstUpperCode, alphabetSize) {
+    return (
+      firstUpperCode +
+      ((this.getLatinUpperCharIndex(chr, firstUpperCode) -
+        this.getLatinUpperCharIndex(keyChr, firstUpperCode) +
+        alphabetSize) %
+        alphabetSize)
+    );
+  }
+  isLatin(chr) {
+    return chr.search(/[a-zA-Z]/) !== -1;
+  }
+  crypt(str, key, dir) {
+    if (str === undefined || key === undefined) {
+      throw Error("Error! Expected 2 params!");
+    }
+    const strArr = str.split("");
+    const keyArr = key
+      .repeat(Math.max(Math.abs(Math.ceil(str.length - key.length)), 1))
+      .split("");
+    const result = [];
+    let keyIndex = 0;
+    strArr.forEach((el, i) => {
+      if (this.isLatin(el)) {
+        const newSymbol =
+          dir === "encrypt"
+            ? this.calcCodeCipherSymbol(el, keyArr[keyIndex], 65, 26)
+            : this.calcDecodeCipherSymbol(el, keyArr[keyIndex], 65, 26);
+        result.push(String.fromCharCode(newSymbol));
+        keyIndex++;
+      } else {
+        result.push(el);
+      }
+    });
+    return this.isDirect ? result.join("") : result.reverse().join("");
+  }
+  encrypt(encodeStr, key) {
+    return this.crypt(encodeStr, key, "encrypt");
+  }
+  decrypt(decodeStr, key) {
+    return this.crypt(decodeStr, key, "decrypt");
   }
 }
 
